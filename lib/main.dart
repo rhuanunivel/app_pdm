3b394930d57da47792bf1c92b929ea6fd32bad8a
RHUAN MOTA MULLER <RMMULLER@UNIVEL.UNIVEL.BR>
2026-03-23 21:37:52 -0300

    teste

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: Center(
+            child: Text("Meu APP"),
+          )
+        ),
+        body: Center(
+          child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text("Hellou World"),
+            Text("23/03/2026")
+          ],
+          )
+        )
+      )
+    )
+  );
+}
